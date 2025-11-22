0421635ee00d0825d9e9886ea8117b51c2a30275
juniorpen01 <01juniorpen@gmail.com>
2025-11-22 21:04:05 +0800

    Follow Tutorial 2, which guides on data types

---
+++ cmd/tutorial_2/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"unicode/utf8"
+)
+
+func main() {
+	// Overengineered crap
+	var p = func(a ...any) {
+		if _, err := fmt.Println(a); err != nil {
+			log.Fatalf("Unable to print line: %+v", err)
+		}
+	}
+
+	{
+		var foo uint16 = 65535
+		bar := foo + 1
+
+		p(foo)
+		p(bar)
+	}
+	{
+		foo := 12345678.9
+
+		p(foo)
+		p(float32(foo))
+	}
+	{
+		foo := 3
+		bar := 2
+
+		p(foo / bar)
+		p(foo % bar)
+	}
+	{
+		foo := 'Æ'
+		bar := "Æ°"
+
+		p(string(foo))
+		p(bar)
+
+		p(len(bar))
+		p(utf8.RuneCountInString(bar))
+		p(len([]rune(bar)))
+	}
+	{
+		foo := false
+
+		p(foo)
+	}
+}
