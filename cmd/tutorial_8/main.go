d7ce68f6e99665ad01adeba25a90ac55fe611f3d
juniorpen01 <01juniorpen@gmail.com>
2025-11-26 17:18:43 +0800

    Follow through the rest of Tutorial 8, going through mutexes

--- cmd/tutorial_8/main.go
+++ cmd/tutorial_8/main.go
@@ -8,7 +8,8 @@ import (
 	"time"
 )
 
-var wg = sync.WaitGroup{}
+var wg sync.WaitGroup
+var mu sync.RWMutex
 
 type datum struct {
 	id int
@@ -18,23 +19,36 @@ func (d datum) String() string {
 	return fmt.Sprintf("Id: %d", d.id)
 }
 
-func (d datum) process() {
+func (d datum) process(out *[]int) {
 	delay := rand.Intn(6)
 	time.Sleep(time.Duration(delay) * time.Second)
+
 	log.Println(d)
+
+	mu.Lock()
+	*out = append(*out, d.id)
+	mu.Unlock()
+
+	// mu.RLock()
+	// log.Println(*out)
+	// mu.RUnlock()
+
 	wg.Done()
 }
 
-var data = [...]datum{{1}, {2}, {3}, {4}, {5}}
+var data = [1000]datum{{1}, {2}, {3}, {4}, {5}}
 
 func main() {
 	now := time.Now()
 
+	out := make([]int, 0, len(data))
+
 	for _, datum := range data {
 		wg.Add(1)
-		go datum.process()
+		go datum.process(&out)
 	}
 	wg.Wait()
 
 	log.Printf("Total execution time: %v\n", time.Since(now))
+	log.Println(out)
 }
